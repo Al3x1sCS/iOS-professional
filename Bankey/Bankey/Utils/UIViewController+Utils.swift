47b54bb5a489b72bfd0b50ac7bf97d96a150819f
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-08-11 16:30:54 -0300

    feat: Adicionando Tab bar.

---
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by user on 11/08/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
