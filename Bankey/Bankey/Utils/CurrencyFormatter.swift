16614fc737ff4b66de7dbe05e9b0cc7f3c291395
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-08-19 10:48:38 -0300

    refactor: Minor Refactor.

--- Bankey/Bankey/Utils/CurrencyFormatter.swift
+++ Bankey/Bankey/Utils/CurrencyFormatter.swift
@@ -25,19 +25,20 @@ struct CurrencyFormatter {
     }
     
     // Converts 929466 > 929,466
-    private func convertDollar(_ dollarPart: Double) -> String {
+    func convertDollar(_ dollarPart: Double) -> String {
         let dollarsWithDecimal = dollarsFormatted(dollarPart) // "$929,466.00"
         let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        
         let decimalSeparator = formatter.decimalSeparator! // "."
         let dollarComponents = dollarsWithDecimal.components(separatedBy: decimalSeparator) // "$929,466" "00"
         var dollars = dollarComponents.first! // "$929,466"
         dollars.removeFirst() // "929,466"
-
         return dollars
     }
     
     // Convert 0.23 > 23
-    private func convertCents(_ centPart: Double) -> String {
+    func convertCents(_ centPart: Double) -> String {
         let cents: String
         if centPart == 0 {
             cents = "00"
@@ -50,6 +51,8 @@ struct CurrencyFormatter {
     // Converts 929466 > $929,466.00
     func dollarsFormatted(_ dollars: Double) -> String {
         let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        
         formatter.numberStyle = .currency
         formatter.usesGroupingSeparator = true
         
@@ -65,7 +68,7 @@ struct CurrencyFormatter {
         let dollarAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .title1)]
         let centAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
         
-        let rootString = NSMutableAttributedString(string: "", attributes: dollarSignAttributes)
+        let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
         let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
         let centString = NSAttributedString(string: cents, attributes: centAttributes)
         
